8a828c1402948381ebe99979c9be6d59169f6516
Wojciech Gunia <you@example.com>
2024-12-31 21:41:17 +0100

    JTL7-41 Complete project initialization

---
+++ material.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+
+@NgModule({
+  exports: [MatTabsModule],
+})
+export class MaterialModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './modules/core/core.module';
+import { JsontoolsModule } from './modules/jsontools/jsontools.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    JsontoolsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
